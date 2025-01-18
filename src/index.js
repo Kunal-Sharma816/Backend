bfe65e0e04c3c957e2e82fd97e665c3ecdc7f50a
Kunal-Sharma816 <22bcs058@iiitdwd.ac.in>
2025-01-18 11:51:55 +0530

    DB connection

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// require('dotenv').config({path: './env'});
+
+import  dotenv  from "dotenv";
+// import mongoose  from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+/* Approch 1
+
+import express from "express";
+const app  = express()
+
+(async ()=>
+{
+    try{
+
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error" , (error)=>
+        {
+            console.log("ERROR: ", error);
+            throw err
+        })
+
+        app.listen(process.env.PORT  , ()=>
+        {
+            console.log(`App is listening on port: ${process.env.PORT}`)
+        })
+
+    } catch(error)
+    {
+        console.log("ERROR: ", error)
+        throw err
+    }
+
+})()
+*/
